2f9f40db8a2c32b5bb851b75d725eee454ddcfcb
HLoghnan <143848580+GamingN0body@users.noreply.github.com>
2024-05-15 12:23:17 -0700

    I created a system of dark mode

---
+++ app.js
@@ -0,0 +1,42 @@
+{/*     <div class="container">
+        <h1>Theme Toggling Demo</h1>
+        <button id="toggle-theme">Toggle Theme</button>
+    </div> */}
+
+const cssStyleSheet = document.getElementById("theme-css") //gets the reference to css to toggle page
+const toggleBtn = document.getElementById("toggle-theme") //gets the reference to toggle page
+
+if (toggleBtn) toggleBtn.addEventListener('click', changeTheme);//to make sure there is no errors
+
+function changeTheme() {
+    //get current theme
+    const oldTheme = getTheme();
+
+    //invert the theme
+    let newTheme;
+
+    if (oldTheme === "dark") {
+        newTheme = "light";
+    } else newTheme = "dark";
+
+    localStorage.setItem('theme', newTheme);
+    cssStyleSheet.href = `./css/${newTheme}.css`;
+
+    //save the new theme and show it
+}
+
+function getTheme() {
+    let theme = localStorage.getItem("theme") //returns the value if not exists it returns null
+
+    if (theme === null) {
+        theme = "dark";
+        localStorage.setItem("theme", theme);
+    }
+
+    return theme; //returns either dark or light
+}
+
+(() => {
+    let theme = getTheme();
+    cssStyleSheet.href = `./css/${theme}.css`;
+})()
